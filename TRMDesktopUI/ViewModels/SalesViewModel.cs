cccc60548cef40ec0e6d8917cef52ea6130807f3
Emperor-Jordan <Jordan.Ton9@gmail.com>
2023-07-27 17:54:40 -0700

    Add Wire Up the WPF Shopping Cart.

--- TRMDesktopUI/ViewModels/SalesViewModel.cs
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -16,9 +16,11 @@ namespace TRMDesktopUI.ViewModels
 
         private BindingList<ProductModel> _products;
 
-        private int _itemQuantity;
+        private ProductModel _selectedProduct;
 
-        private BindingList<ProductModel> _cart;
+        private int _itemQuantity = 1;
+
+        private BindingList<CartItemModel> _cart = new BindingList<CartItemModel>();
 
         public SalesViewModel(IProductEndpoint productEndpoint)
         {
@@ -50,6 +52,20 @@ namespace TRMDesktopUI.ViewModels
             }
         }
 
+        public ProductModel SelectedProduct
+        {
+            get { return _selectedProduct; }
+            set 
+            { 
+                _selectedProduct = value;
+
+                NotifyOfPropertyChange(() => SelectedProduct);
+
+                NotifyOfPropertyChange(() => CanAddToCart);
+            }
+        }
+
+
         public int ItemQuantity
         {
             get { return _itemQuantity; }
@@ -58,10 +74,12 @@ namespace TRMDesktopUI.ViewModels
                 _itemQuantity = value;
 
                 NotifyOfPropertyChange(() => ItemQuantity);
+
+                NotifyOfPropertyChange(() => CanAddToCart);
             }
         }    
 
-        public BindingList<ProductModel> Cart
+        public BindingList<CartItemModel> Cart
         {
             get { return _cart; }
             set 
@@ -76,8 +94,14 @@ namespace TRMDesktopUI.ViewModels
         {
             get
             {
-                // TODO - Replace with calculation
-                return "$0.00";
+                decimal subTotal = 0;
+
+                foreach (var item in Cart)
+                {
+                    subTotal += (item.Product.RetailPrice * item.QuantityInCart);
+                }
+
+                return subTotal.ToString("C");
             }
         }
 
@@ -106,12 +130,10 @@ namespace TRMDesktopUI.ViewModels
                 bool output = false;
 
                 // Make sure something is selected
-
-                // Make sure there is an item quantity
-
-                if (true)
+                // Make sure there is an item quantity, and item quantity cannot be 0
+                if (ItemQuantity > 0 && SelectedProduct?.QuantityInStock >= ItemQuantity)
                 {
-
+                    output = true;
                 }
 
                 return output;
@@ -120,7 +142,33 @@ namespace TRMDesktopUI.ViewModels
 
         public void AddToCart()
         {
+            CartItemModel existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
 
+            if (existingItem != null)
+            {
+                existingItem.QuantityInCart += ItemQuantity;
+
+                // HACK - There should be a better way of refreshing the cart display
+                Cart.Remove(existingItem);
+
+                Cart.Add(existingItem);
+            }
+            else
+            {
+                CartItemModel item = new CartItemModel
+                {
+                    Product = SelectedProduct,
+                    QuantityInCart = ItemQuantity
+                };
+
+                Cart.Add(item);
+            }
+
+            SelectedProduct.QuantityInStock -= ItemQuantity;
+
+            ItemQuantity = 1;
+
+            NotifyOfPropertyChange(() => SubTotal);
         }
 
         public bool CanRemoveFromCart
@@ -142,7 +190,7 @@ namespace TRMDesktopUI.ViewModels
 
         public void RemoveFromCart()
         {
-
+            NotifyOfPropertyChange(() => SubTotal);
         }
 
         public bool CanCheckOut
