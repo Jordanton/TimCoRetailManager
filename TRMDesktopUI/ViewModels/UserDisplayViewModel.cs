84be6f6b3da77353b96111ed242e4da1b6b13832
Emperor-Jordan <Jordan.Ton9@gmail.com>
2023-08-29 17:54:13 -0700

    Add managing user roles part 2.

--- TRMDesktopUI/ViewModels/UserDisplayViewModel.cs
+++ TRMDesktopUI/ViewModels/UserDisplayViewModel.cs
@@ -22,6 +22,18 @@ namespace TRMDesktopUI.ViewModels
 
         private BindingList<UserModel> _users;
 
+        private UserModel _selectedUser;
+
+        private string _selectedRoleToRemove;
+
+        private string _selectedRoleToAdd;
+
+        private string _selectedUserName;
+
+        private BindingList<string> _selectedUserRoles = new BindingList<string>();
+
+        private BindingList<string> _availableRoles = new BindingList<string>();
+
         public BindingList<UserModel> Users
         {
             get { return _users; }
@@ -30,6 +42,78 @@ namespace TRMDesktopUI.ViewModels
                 _users = value;
 
                 NotifyOfPropertyChange(() => Users);
+            }
+        }
+
+        public UserModel SelectedUser
+        {
+            get { return _selectedUser; }
+            set 
+            { 
+                _selectedUser = value;
+
+                SelectedUserName = value.Email;
+
+                SelectedUserRoles = new BindingList<string>(value.Roles.Select(x => x.Value).ToList());
+
+                LoadRoles();
+
+                NotifyOfPropertyChange(() => SelectedUser);
+            }
+        }       
+
+        public string SelectedRoleToRemove
+        {
+            get { return _selectedRoleToRemove; }
+            set 
+            { 
+                _selectedRoleToRemove = value;
+
+                NotifyOfPropertyChange(() => SelectedRoleToRemove);
+            }
+        }       
+
+        public string SelectedRoleToAdd
+        {
+            get { return _selectedRoleToAdd; }
+            set 
+            { 
+                _selectedRoleToAdd = value;
+
+                NotifyOfPropertyChange(() => SelectedRoleToAdd);
+            }
+        }
+
+        public string SelectedUserName
+        {
+            get { return _selectedUserName; }
+            set 
+            { 
+                _selectedUserName = value;
+
+                NotifyOfPropertyChange(() => SelectedUserName);
+            }
+        }
+
+        public BindingList<string> SelectedUserRoles
+        {
+            get { return _selectedUserRoles; }
+            set 
+            { 
+                _selectedUserRoles = value;
+
+                NotifyOfPropertyChange(() => SelectedUserRoles);
+            }
+        }
+
+        public BindingList<string> AvailableRoles
+        {
+            get { return _availableRoles; }
+            set 
+            { 
+                _availableRoles = value;
+
+                NotifyOfPropertyChange(() => AvailableRoles);
             }
         }
 
@@ -78,6 +162,37 @@ namespace TRMDesktopUI.ViewModels
             var userList = await _userEndpoint.GetAll();
 
             Users = new BindingList<UserModel>(userList);
+        }
+
+        private async Task LoadRoles()
+        {
+            var roles = await _userEndpoint.GetAllRoles();
+
+            foreach (var role in roles)
+            {
+                if (SelectedUserRoles.IndexOf(role.Value) < 0)
+                {
+                    AvailableRoles.Add(role.Value);
+                }
+            }
+        }
+
+        public async Task AddSelectedRole()
+        {
+            await _userEndpoint.AddUserToRole(SelectedUser.Id, SelectedRoleToAdd);
+
+            SelectedUserRoles.Add(SelectedRoleToAdd);
+
+            AvailableRoles.Remove(SelectedRoleToAdd);
+        }
+
+        public async Task RemoveSelectedRole()
+        {
+            await _userEndpoint.RemoveUserFromRole(SelectedUser.Id, SelectedRoleToRemove);
+
+            AvailableRoles.Add(SelectedRoleToRemove);
+
+            SelectedUserRoles.Remove(SelectedRoleToRemove);
         }
     }
 }
